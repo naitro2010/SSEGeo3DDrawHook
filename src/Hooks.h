4fb2b47a805087b9378095492e96726accb2eb46
naitro2010 <108594252+naitro2010@users.noreply.github.com>
2025-02-10 02:49:10 -0800

    fix bugs

--- src/Hooks.h
+++ src/Hooks.h
@@ -1,9 +1,9 @@
 #pragma once
-
+#include <stdint.h>
 namespace plugin {
     class Hooks {
         private:
-            static void Draw(uint64_t arg1,uint32_t arg2,uint8_t arg3)
+            static void Draw(uint64_t arg1,uint32_t arg2,uint8_t arg3);
             static inline REL::Relocation<decltype(Draw)> origDraw;
 
         public:
